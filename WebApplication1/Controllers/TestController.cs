8adf914bd4322722ec71e5e3c766848573ecee78
Ilya Myshkin <hak.dok0@gmail.com>
2024-07-22 20:05:24 +0300

    chore: установил design пакет для миграций в DAL. За одно проверяю gitignore

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -1,23 +1,19 @@
 ﻿using DAL;
-using DAL.EfClasses;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace MimicWebApi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TestController : ControllerBase
-    {
-        private UnitOfWork unitOfWork = new UnitOfWork();
+namespace MimicWebApi.Controllers;
 
-        [HttpPost]
-        public IActionResult Create([FromBody] UserDTO user)
-        {
-            unitOfWork.UserRepository.Insert(user.ToUser());
-            unitOfWork.Save();
-            return Ok();
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class TestController(UnitOfWork unitOfWork) : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Create([FromBody] UserDTO user)
+    {
+        unitOfWork.UserRepository.Insert(user.ToUser());
+        unitOfWork.Save();
+        return Ok();
     }
 }
