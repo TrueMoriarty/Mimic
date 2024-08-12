f0f91cd930a79b23cc9004962ca234395b63af24
Bagiev Arslan <arslan.bagiev@gmail.com>
2024-08-12 18:04:48 +0300

    refactor: add AsNoTracking to GetPaged method in ItemRepository

--- DAL/Repositories/ItemRepository.cs
+++ DAL/Repositories/ItemRepository.cs
@@ -1,6 +1,7 @@
 using DAL.Dto;
 using DAL.EfClasses;
 using DAL.EfCode;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories;
 
@@ -19,7 +20,7 @@ public class ItemRepository(MimicContext context) : GenericRepository<Item>(cont
 			|| item.Name == paginateDataItemDto.SearchString)
 			.Skip((paginateDataItemDto.PageIndex - 1) 
 			* paginateDataItemDto.PageSize)
-			.Take(paginateDataItemDto.PageSize);
+			.Take(paginateDataItemDto.PageSize).AsNoTracking();
 
 		List<Item> result = 
 			paginateDataItemDto.OrderBy == nameof(Item.Name) 
