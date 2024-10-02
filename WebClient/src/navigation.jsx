b8ccd3cf0eb91e269116984251ff6ad9ff7810eb
Bagiev Arslan <arslan.bagiev@gmail.com>
2024-10-02 16:18:48 +0300

    refactor: check tab

--- WebClient/src/navigation.jsx
+++ WebClient/src/navigation.jsx
@@ -51,7 +51,7 @@ const Navigation = () => {
                             <Tabs
                                 value={currentTab}
                                 sx={{ mb: 0.5 }}
-                                textColor='mimic'
+                                textColor='inherit'
                                 TabIndicatorProps={{
                                     style: {
                                         background: '#6E8190',
